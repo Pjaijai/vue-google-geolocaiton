d60a09e51ded97a4f9f0384b892e2d875e4ddb46
Pjaijai <paul6a24@hotmail.com>
2023-08-05 19:22:04 -0400

    Adding goole map

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,17 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueGoogleMaps from '@fawmi/vue-google-maps'
 
 const app = createApp(App)
 
-app.use(router)
-
-app.mount('#app')
+app
+  .use(VueGoogleMaps, {
+    load: {
+      key: import.meta.env.VUE_APP_GOOGLE_API_KEY
+    },
+    autobindAllEvents: true,
+    libraries: 'places'
+  })
+  .use(router)
+  .mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,73 @@
-<script setup lang="ts"></script>
-
 <template>
-  <main></main>
+  <main>
+    <button @click="changeTemp('asdasd')">123</button>
+    <button @click.prevent="handleGetLocation">search your location</button>
+    <h1>{{ temp }}</h1>
+    <h2>latitude:{{ center.lat }}</h2>
+    <h2>longitude:{{ center.lng }}</h2>
+    <GMapAutocomplete placeholder="This is a placeholder" @place_changed="setPlace">
+    </GMapAutocomplete>
+    <GMapMap
+      :center="{ lat: center.lat, lng: center.lng }"
+      :zoom="10"
+      style="width: 100vw; height: 20rem"
+    >
+      <GMapMarker
+        :key="index"
+        v-for="(m, index) in markers"
+        :position="m.position"
+        :clickable="true"
+        :draggable="true"
+      />
+    </GMapMap>
+  </main>
 </template>
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'HomeView',
+  setup() {
+    const temp = ref<string>('temp1')
+    const changeTemp = (value: string) => {
+      temp.value = value
+    }
+    const center = ref<{ lat: number | undefined; lng: number | undefined }>({
+      lat: undefined,
+      lng: undefined
+    })
+    const markers = ref([
+      {
+        position: {
+          lat: center.value.lat,
+          lng: center.value.lng
+        }
+      }
+    ])
+
+    const handleGetLocation = async () => {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          center.value.lat = position.coords.latitude
+          center.value.lng = position.coords.longitude
+
+          markers.value = [
+            {
+              position: {
+                lat: center.value.lat,
+                lng: center.value.lng
+              }
+            }
+          ]
+        },
+        (error) => {
+          console.error(error.message)
+        }
+      )
+    }
+
+    const setPlace = (e) => {}
+    return { temp, changeTemp, handleGetLocation, center, markers, setPlace }
+  }
+})
+</script>
